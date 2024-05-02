fc4f9e4288843bcf40406bd74e5898331194a60c
nikol <nikolaigricak@gmail.com>
2024-05-02 22:33:10 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace С_6_Calendar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
